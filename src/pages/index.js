d387d13fe2422e63391393ef1956c1f31a3fbed8
GetYourAlbatross <31719531+GetYourAlbatross@users.noreply.github.com>
2025-05-03 22:06:44 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-// Step 1: Import React
+lcome// Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
 
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial</p>
+      <p>Welcome</p>
     </Layout>
   )
 }
